e17124693d96a002ff166e385b50a88694fb6947
ACLTC <acltc@ACLTCs-MacBook-Air.local>
2015-11-15 16:42:11 -0800

    added custom methods full_name and friendly_created_at

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,16 @@
 class Contact < ActiveRecord::Base
+
+
+  def friendly_created_at
+    created_at.strftime("%A, %d %b %Y %l:%M %p")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japanese_phone_prefix
+    "+81 #{phone}"
+  end
+
 end
